5a661f30d27149e8650e178be8e158d0857221ab
Ikem Nomso <ikemnomso@gmail.com>
2024-05-13 21:07:21 +0100

    build(dependencies): install required packages

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+const { PORT } = process.env;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
