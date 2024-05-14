b9f3205bdd920c40dfe8ea9e4ef427078cf2b5b2
Ikem Nomso <ikemnomso@gmail.com>
2024-05-14 09:54:53 +0100

    chore(database): add database connection

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
-require("dotenv").config();
-const app = express();
+const { connectToMongoDB } = require("./database/connection");
 
+require("dotenv").config();
+
+const app = express();
 const { PORT } = process.env;
+
+connectToMongoDB();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
