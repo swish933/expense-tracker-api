0f71ac58af2bc7e3ac9c1c43f790aa47d571aa9e
Ikem Nomso <ikemnomso@gmail.com>
2024-05-16 23:02:12 +0100

    refactor(services): add error handling logic for services

--- services/user.service.js
+++ services/user.service.js
@@ -2,23 +2,31 @@ const User = require("../database/schemas/user.schema");
 const { ErrorWithStatus } = require("../exceptions/error_with_status");
 
 const getUser = async (userId) => {
-	const user = await User.findById(userId);
+	try {
+		const user = await User.findById(userId);
 
-	if (!user) {
-		throw new ErrorWithStatus("User not found", 404);
+		if (!user) {
+			throw new ErrorWithStatus("User not found", 404);
+		}
+
+		return user;
+	} catch (error) {
+		throw new ErrorWithStatus(error.message, 500);
 	}
-
-	return user;
 };
 
 const updateUser = async (userId, dto) => {
-	const updatedUserInfo = await User.findByIdAndUpdate(
-		userId,
-		{ ...dto },
-		{ new: true }
-	);
+	try {
+		const updatedUserInfo = await User.findByIdAndUpdate(
+			userId,
+			{ ...dto },
+			{ new: true }
+		);
 
-	return updatedUserInfo;
+		return updatedUserInfo;
+	} catch (error) {
+		throw new ErrorWithStatus(error.message, 500);
+	}
 };
 
 module.exports = { getUser, updateUser };
