17140ae587b1431417924d6820133dd41b5cd3c4
Ikem Nomso <ikemnomso@gmail.com>
2024-05-16 11:15:02 +0100

    refactor(error handler): consolidate error handling logic

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { connectToMongoDB } = require("./database/connection");
-const authRouter = require("./routers/auth.router.js");
+const authRouter = require("./routers/auth.router");
 
 require("dotenv").config();
 
@@ -11,11 +11,12 @@ app.use(express.json());
 
 connectToMongoDB();
 
-app.use((err, req, res, next) => {
-	console.error(err);
-	res.status(err.status || 500);
-	res.json({ error: err.message });
+app.use("/auth", authRouter);
+
+app.use((error, req, res, next) => {
+	console.error(error);
+	res.status(error.status || 500);
+	res.json({ error: error.message });
 });
 
-app.use("/auth", authRouter);
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
