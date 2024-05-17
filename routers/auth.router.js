3c5c7d5c37fa6fe1cad831f94bcb84d5f48e79d5
Nomso Ikem <ikemnomso@gmail.com>
2024-05-17 15:07:45 +0100

    Revert "Feat/expenses setup"

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,13 +1,10 @@
-const { Router } = require('express');
-const authController = require('../controllers/auth.controller');
-const {
-  signupValidation,
-  loginValidation,
-} = require('../validation/auth.validation');
+const { Router } = require("express");
+const authController = require("../controllers/auth.controller");
+const { signupValidation, loginValidation } = require("../validation/auth.validation");
 
 const authRouter = Router();
 
-authRouter.post('/signup', signupValidation, authController.signup);
-authRouter.post('/login', loginValidation, authController.login);
+authRouter.post("/signup", signupValidation, authController.signup);
+authRouter.post("/login", loginValidation, authController.login);
 
 module.exports = authRouter;
