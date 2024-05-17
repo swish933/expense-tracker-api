8543304936994d9fdd205f512698434de9d870ff
Nomso Ikem <ikemnomso@gmail.com>
2024-05-17 15:07:45 +0100

    Revert "Feat/expenses setup"

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,27 +1,29 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const isAuthenticated = (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if (!authorization) {
-    return res.status(401).send({ message: 'Unauthorized' });
-  }
+const authMiddleware = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (!authorization) {
+		return res.status(401).send({ message: "Unauthorized" });
+	}
 
-  const bearerToken = authorization.split(' ');
-  if (bearerToken.length !== 2) {
-    return res.status(401).send({ message: 'Unauthorized' });
-  }
+	const bearerToken = authorization.split(" ");
+	if (bearerToken.length !== 2) {
+		return res.status(401).send({ message: "Unauthorized" });
+	}
 
-  const token = bearerToken[1];
+	const token = bearerToken[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded;
+		req.user = decoded;
 
-    next();
-  } catch (error) {
-    return res.status(401).send({ message: error.message || 'Unauthorized' });
-  }
+		next();
+	} catch (error) {
+		console.error(error);
+
+		return res.status(401).send({ message: "Unauthorized" });
+	}
 };
 
-module.exports = isAuthenticated;
+module.exports = authMiddleware;
--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-const express = require('express');
-const { connectToMongoDB } = require('./database/connection');
-const authRouter = require('./routers/auth.router.js');
-const expenseRouter = require('./routers/expense.router.js');
-const isAuthenticated = require('./middlewares/auth.middleware');
+const express = require("express");
+const { connectToMongoDB } = require("./database/connection");
+const authRouter = require("./routers/auth.router");
 const userRouter = require("./routers/user.router");
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const { PORT } = process.env;
 
 app.use(express.json());
 
-app.use('/api/auth', authRouter);
-app.use("api/me", userRouter);
-app.use(isAuthenticated);
-app.use('/api', expenseRouter);
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500);
-  res.json({ error: err.message });
-});
-
 connectToMongoDB();
 
+app.use("/auth", authRouter);
+app.use("/me", userRouter);
+
+app.use((error, req, res, next) => {
+	console.error(error);
+	res.status(error.status || 500);
+	res.json({ error: error.message, success: false });
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
